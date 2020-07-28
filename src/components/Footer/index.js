55ede2fa14c8a31e98ea843ba2b86d141cd3bd17
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-28 16:36:33 -0300

    Footer added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import {FaHeart} from 'react-icons/fa';
 
 function Footer() {
   return (
@@ -8,7 +9,7 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        made with <FaHeart /> by JVMedeiros in 
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
