5c5365823e639e74c09c9fa3453346919540a34c
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-28 14:54:58 -0300

    Button component created

---
+++ src/components/Menu/ButtonLink/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ButtonLink () {
+    return (
+        <a className="" href="/">
+            Novo Vídeo
+        </a>
+    );
+}
+
+export default ButtonLink;
