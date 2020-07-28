046e1d9b6e963102e3fbe36f9e097f384b564f43
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-28 14:33:43 -0300

    logo added

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+//Assets
+import Logo from '../../assets/img/logo.png'
+
 function Menu() {
     return (
         <header>
-            DevFlix
+            <img src={Logo} alt="Logo"/>
         </header>
     );
 }
