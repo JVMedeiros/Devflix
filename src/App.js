fd4150bb0f8a227f8fbe594f87f08199869d0c69
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-28 15:30:02 -0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import React from 'react';
 //Components
 import Menu from './components/Menu';
 
+
 function App() {
   return (
-    <div>
       <Menu />
-      Hello world
-    </div>
   );
 }
 
