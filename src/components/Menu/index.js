3a05e96ab4685cf7d8d2ecbfac9d3b349d13c826
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-28 15:31:02 -0300

    buttonlink setup

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+//Components 
+import ButtonLink from '../ButtonLink';
+
 //Assets
 import Logo from '../../assets/img/logo.png';
 
 //Styles
 import './index.css';
+
 
 function Menu() {
     return (
@@ -12,6 +16,10 @@ function Menu() {
             <a href="/">
                 <img className="Logo" src={Logo} alt="Logo"/>
             </a>
+
+            <ButtonLink clasName="ButtonLink" href="/">
+                Novo Vídeo
+            </ButtonLink>
         </nav>
     );
 }
