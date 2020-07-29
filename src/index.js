edbb2466c035f65e518e5d4e738c7bdfa2d48c5c
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-29 11:08:02 -0300

    route conflict resolve

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
 
+//Router manager 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} />
+      <Route path="/" component={Home} exact/>
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
